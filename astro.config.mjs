b4ed3b4146dc64c0e929e3512de41f8b65ed511b
mbvissers <mb.vissers@gmail.com>
2025-02-18 23:02:21 +0100

    robots.txt

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,9 +2,14 @@
 import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "astro/config";
 
+import sitemap from "@astrojs/sitemap";
+
 // https://astro.build/config
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()],
   },
+
+  site: "https://www.mbvissers.com/",
+  integrations: [sitemap()],
 });
